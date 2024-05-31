a789cf892997078ab21f8036d26589f172bd3bdd
Cristian Morales Hammouche <cristian96.cm@gmail.com>
2024-05-31 18:00:31 +0000

    caso de uso gold

--- models/marts/stg_events.sql
+++ models/marts/stg_events.sql
@@ -5,13 +5,23 @@ WITH stg_events AS (
 
 renamed_casted AS (
     SELECT
-	       user_id
+	    a.session_id,
+        a.user_id,
+        b.last_name,
+        b.first_name,
+        b.email
         ,sum(case when event_type = 'checkout' then 1 end) as checkout_amount
         ,sum(case when event_type = 'package_shipped' then 1 end) as package_shipped_amount
         ,sum(case when event_type = 'add_to_cart' then 1 end) as add_to_cart_amount
         ,sum(case when event_type = 'page_view' then 1 end) as page_view_amount
-    FROM stg_events
-    GROUP BY 1
+        ,min(a.created_at) as first_event
+        ,max(a.created_at) as last_event
+        ,DATEDIFF(minute,first_event,last_event) as sesion_time_minutes
+    FROM ALUMNO14DEV_SILVER_DB.DBT.STG_SQL_SERVER_DBO__events a
+    inner join ALUMNO14DEV_SILVER_DB.DBT.STG_SQL_SERVER_DBO__USERS b
+    on a.user_id = b.user_id
+    group by a.user_id,a.session_id,b.last_name,b.first_name,b.email
+    
     )
 
 SELECT * FROM renamed_casted
