b621080b02b669dab60be4c44b5ea2bb1062139b
Cristian Morales Hammouche <cristian96.cm@gmail.com>
2024-06-06 09:23:59 +0000

    tabla hechos

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -26,7 +26,30 @@ renamed as (
 
 )
 SELECT 
-*
+a.user_id,
+a.order_id,
+c.product_id,
+quantity ,
+price,
+quantity*price as total_idproducto,
+b.status_description,
+case 
+    when status_description = 'inactive' then 0
+    else discount_dollars
+end as discount_dollars_active,
+discount_dollars as descuento_original,
+ROUND(shipping_cost / COUNT(*) OVER (PARTITION BY a.order_id),2) as shipping_cost_dividido,
+shipping_cost_antiguo,
+order_cost as order_cost_antiguo,
+SUM(quantity * price) OVER (PARTITION BY a.order_id) - 
+    case 
+        when b.status_description = 'inactive' then 0
+        else discount_dollars
+    end as order_cost,
+ROUND(order_total / COUNT(*) OVER (PARTITION BY a.order_id),2) as order_total_divido,
+SUM(order_total) OVER (PARTITION BY a.order_id) / COUNT(*) OVER (PARTITION BY a.order_id) as suma,
+
+
 FROM renamed a
 inner join {{ ref('dim_promos') }} b
 on a.promo_id=b.id_promocion
